c8741bfb9ab0a174e3f6317c7797ed91f86db8e9
Carlos Alan <carlosv@MacBook-Pro-de-Carlos.local>
2024-06-05 12:24:33 -0600

    adding terraform main file and variables needed

---
+++ terraform/main.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "original_images" {
+  bucket = "original-images-bucket"
+}
+resource "aws_s3_bucket" "thumbnails" {
+  bucket = "thumbnails-bucket"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "lambda.amazonaws.com",
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_lambda_function" "thumbnail_generator" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "thumbnail_generator"
+  role             = aws_iam_role.iam_for_lambda.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.8"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "thumbnail-api"
+  description = "API for thumbnail generation"
+}
+
+resource "aws_api_gateway_resource" "resource" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "generate-thumbnail"
+}
+
+resource "aws_api_gateway_method" "method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.resource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.thumbnail_generator.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_integration" "integration" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  resource_id = aws_api_gateway_resource.resource.id
+  http_method = aws_api_gateway_method.method.http_method
+  type        = "AWS_PROXY"
+  uri         = aws_lambda_function.thumbnail_generator.invoke_arn
+}
